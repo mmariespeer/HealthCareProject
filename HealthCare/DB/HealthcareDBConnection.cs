cf8569da6f86af29b68d26a41741ee9dd260d5e2
Megan Brown <mjones29@my.westga.edu>
2019-03-20 12:35:25 -0400

    Added HealthcareDBConnection to allow connection to local database cs6232-g2

---
+++ HealthCare/DB/HealthcareDBConnection.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+
+namespace HealthCare.DB
+{
+    /// <summary>
+    /// Sets up connection to healthcare database (cs6232-g2) on local machine
+    /// </summary>
+    class HealthcareDBConnection
+    {
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=localhost;Initial Catalog=cs6232-g2;" +
+                "Integrated Security=True";
+
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
