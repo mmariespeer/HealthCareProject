2360ed8037c33ad0b7617576ef9957dc2b663145
Megan Brown <mjones29@my.westga.edu>
2019-03-20 12:21:54 -0400

    Added appointment model class. Added DAL, DB, and Controller empty folders.

---
+++ HealthCare/Model/Appointment.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace HealthCare.Model
+{
+    /// <summary>
+    /// Gets and sets all attributes of an appointment
+    /// </summary>
+    public class Appointment
+    {
+        public int appointmentID { get; set; }
+        
+        public int patientID { get; set; }
+
+        public int doctorID { get; set; }
+        
+        public DateTime dateTime { get; set; }
+
+        public string reasonForVisit { get; set; }
+    }
+}
