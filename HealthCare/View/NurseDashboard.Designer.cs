398689c9b70ff3ad443217df69b3be3a514db978
Kevin Sun <kvnsun9@gmail.com>
2019-03-21 18:59:05 -0500

    created initial nurse interface and tab control

---
+++ HealthCare/View/NurseDashboard.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace HealthCare.View
+{
+    partial class NurseDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nurseTabControl = new System.Windows.Forms.TabControl();
+            this.patientTabPage = new System.Windows.Forms.TabPage();
+            this.appointmentTabPage = new System.Windows.Forms.TabPage();
+            this.visitTabPage = new System.Windows.Forms.TabPage();
+            this.nurseTabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nurseTabControl
+            // 
+            this.nurseTabControl.Controls.Add(this.patientTabPage);
+            this.nurseTabControl.Controls.Add(this.appointmentTabPage);
+            this.nurseTabControl.Controls.Add(this.visitTabPage);
+            this.nurseTabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nurseTabControl.Location = new System.Drawing.Point(-3, 0);
+            this.nurseTabControl.Name = "nurseTabControl";
+            this.nurseTabControl.SelectedIndex = 0;
+            this.nurseTabControl.Size = new System.Drawing.Size(802, 418);
+            this.nurseTabControl.TabIndex = 0;
+            // 
+            // patientTabPage
+            // 
+            this.patientTabPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.patientTabPage.Location = new System.Drawing.Point(4, 29);
+            this.patientTabPage.Name = "patientTabPage";
+            this.patientTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.patientTabPage.Size = new System.Drawing.Size(794, 385);
+            this.patientTabPage.TabIndex = 0;
+            this.patientTabPage.Text = "Register/Delete Patient";
+            this.patientTabPage.UseVisualStyleBackColor = true;
+            // 
+            // appointmentTabPage
+            // 
+            this.appointmentTabPage.Location = new System.Drawing.Point(4, 29);
+            this.appointmentTabPage.Name = "appointmentTabPage";
+            this.appointmentTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.appointmentTabPage.Size = new System.Drawing.Size(794, 361);
+            this.appointmentTabPage.TabIndex = 1;
+            this.appointmentTabPage.Text = "Appointment Information";
+            this.appointmentTabPage.UseVisualStyleBackColor = true;
+            // 
+            // visitTabPage
+            // 
+            this.visitTabPage.Location = new System.Drawing.Point(4, 29);
+            this.visitTabPage.Name = "visitTabPage";
+            this.visitTabPage.Size = new System.Drawing.Size(794, 361);
+            this.visitTabPage.TabIndex = 2;
+            this.visitTabPage.Text = "Visit Information";
+            this.visitTabPage.UseVisualStyleBackColor = true;
+            // 
+            // NurseDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nurseTabControl);
+            this.Name = "NurseDashboard";
+            this.Text = "Nurse Dashboard";
+            this.nurseTabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl nurseTabControl;
+        private System.Windows.Forms.TabPage patientTabPage;
+        private System.Windows.Forms.TabPage appointmentTabPage;
+        private System.Windows.Forms.TabPage visitTabPage;
+    }
+}
