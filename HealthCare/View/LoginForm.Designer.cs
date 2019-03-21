64033960afa3eafa1ee03475860527f4a1f0e579
Kevin Sun <kvnsun9@gmail.com>
2019-03-21 18:49:12 -0500

    created initial login form view and functionality

---
+++ HealthCare/View/LoginForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace HealthCare.View
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.usernameLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.messageLabel, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.passwordTextBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.loginButton, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.usernameTextBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.passwordLabel, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(79, 49);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27.29045F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27.29045F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.26761F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34.2723F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(293, 213);
+            this.tableLayoutPanel1.TabIndex = 9;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(3, 0);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(58, 13);
+            this.usernameLabel.TabIndex = 2;
+            this.usernameLabel.Text = "Username:";
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(149, 116);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(35, 13);
+            this.messageLabel.TabIndex = 6;
+            this.messageLabel.Text = "label1";
+            this.messageLabel.Visible = false;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(149, 61);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(107, 20);
+            this.passwordTextBox.TabIndex = 2;
+            this.passwordTextBox.TextChanged += new System.EventHandler(this.passwordTextBox_TextChanged);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(149, 142);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 3;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(149, 3);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(107, 20);
+            this.usernameTextBox.TabIndex = 1;
+            this.usernameTextBox.TextChanged += new System.EventHandler(this.usernameTextBox_TextChanged);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(3, 58);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(56, 13);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 342);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Label passwordLabel;
+    }
+}
---
+++ HealthCare/View/LoginForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HealthCare.View
+{
+    public partial class LoginForm : Form
+    {
+       // private MainDashboard md;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+           // md = new MainDashboard(this);
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if ((usernameTextBox.Text == ) && (passwordTextBox.Text == ))
+            {
+                Hide();
+                usernameTextBox.Clear();
+                passwordTextBox.Clear();
+               // md.Show();
+            }
+            else
+            {
+                messageLabel.Text = "invalid username/password";
+                messageLabel.ForeColor = Color.Red;
+                messageLabel.Visible = true;
+            }
+
+        }
+
+
+        private void usernameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (messageLabel.Visible == true)
+            {
+                messageLabel.Visible = false;
+            }
+        }
+
+        private void passwordTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (messageLabel.Visible == true)
+            {
+                messageLabel.Visible = false;
+            }
+        }
+
+
+
+    }
+}
