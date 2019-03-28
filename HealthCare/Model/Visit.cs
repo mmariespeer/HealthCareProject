0a19d7aa52c79dd4b1939f71312567b815de05b9
Melissa Speer Osborne <mspeer1@my.westga.edu>
2019-03-28 09:29:49 -0400

    Added visit base model class

---
+++ HealthCare/Model/Visit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Model
+{
+    class Visit
+    {
+        public int VisitID { get; set; }
+        public int NurseID { get; set; }
+        public int AppointmentID { get; set; }
+        public decimal Weight { get; set; }
+        public int SystolicBP { get; set; }
+        public int DiastolicBP { get; set; }
+        public decimal Temp { get; set; }
+        public int Pulse { get; set; }
+        public string Symptoms { get; set; }
+    }
+}
