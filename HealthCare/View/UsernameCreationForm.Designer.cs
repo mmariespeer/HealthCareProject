3786d21dcf02c6064bdb2728a3273f2091b5671b
Megan Brown <mjones29@my.westga.edu>
2019-04-21 21:37:42 -0400

    Set the creationform control box to false, so that the user is forced t enter a username and password

--- HealthCare/View/UsernameCreationForm.Designer.cs
+++ HealthCare/View/UsernameCreationForm.Designer.cs
@@ -35,16 +35,19 @@
             // 
             this.newLoginUserControl.Dock = System.Windows.Forms.DockStyle.Fill;
             this.newLoginUserControl.Location = new System.Drawing.Point(0, 0);
+            this.newLoginUserControl.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
             this.newLoginUserControl.Name = "newLoginUserControl";
-            this.newLoginUserControl.Size = new System.Drawing.Size(639, 418);
+            this.newLoginUserControl.Size = new System.Drawing.Size(240, 175);
             this.newLoginUserControl.TabIndex = 0;
             // 
             // UsernameCreationForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(639, 418);
+            this.ClientSize = new System.Drawing.Size(240, 175);
+            this.ControlBox = false;
             this.Controls.Add(this.newLoginUserControl);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.Name = "UsernameCreationForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Username and Password";
