c90f66559d1ae8fbbf751bd13ebfcfbc3c252778
Megan Brown <mjones29@my.westga.edu>
2019-04-24 21:14:14 -0400

    Changed dataset

--- HealthCare/UserControls/MostPeformedTestsUserControl.Designer.cs
+++ HealthCare/UserControls/MostPeformedTestsUserControl.Designer.cs
@@ -30,46 +30,20 @@
         {
             this.components = new System.ComponentModel.Container();
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
-            this.spMostPerformedTestsBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.mostperformed = new HealthCare.mostperformed();
-            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
-            this.spMostPerformedTestsTableAdapter = new HealthCare.mostperformedTableAdapters.spMostPerformedTestsTableAdapter();
             this.instructionsLabel = new System.Windows.Forms.Label();
             this.endDate = new System.Windows.Forms.DateTimePicker();
             this.startDate = new System.Windows.Forms.DateTimePicker();
             this.generateReportButton = new System.Windows.Forms.Button();
             this.toLabel = new System.Windows.Forms.Label();
             this.fromLabel = new System.Windows.Forms.Label();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this._cs6232_g2DataSet = new HealthCare._cs6232_g2DataSet();
+            this.spMostPerformedTestsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.spMostPerformedTestsTableAdapter = new HealthCare._cs6232_g2DataSetTableAdapters.spMostPerformedTestsTableAdapter();
+            this.spMostPerformedTestsTableAdapter1 = new HealthCare.mostperformedTableAdapters.spMostPerformedTestsTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this._cs6232_g2DataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.spMostPerformedTestsBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.mostperformed)).BeginInit();
             this.SuspendLayout();
-            // 
-            // spMostPerformedTestsBindingSource
-            // 
-            this.spMostPerformedTestsBindingSource.DataMember = "spMostPerformedTests";
-            this.spMostPerformedTestsBindingSource.DataSource = this.mostperformed;
-            // 
-            // mostperformed
-            // 
-            this.mostperformed.DataSetName = "mostperformed";
-            this.mostperformed.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // reportViewer1
-            // 
-            reportDataSource1.Name = "MostPerformed";
-            reportDataSource1.Value = this.spMostPerformedTestsBindingSource;
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
-            this.reportViewer1.LocalReport.ReportEmbeddedResource = "HealthCare.MostPerformedTests.rdlc";
-            this.reportViewer1.Location = new System.Drawing.Point(-3, 108);
-            this.reportViewer1.Name = "reportViewer1";
-            this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(738, 438);
-            this.reportViewer1.TabIndex = 0;
-            this.reportViewer1.Load += new System.EventHandler(this.reportViewer1_Load);
-            // 
-            // spMostPerformedTestsTableAdapter
-            // 
-            this.spMostPerformedTestsTableAdapter.ClearBeforeFill = true;
             // 
             // instructionsLabel
             // 
@@ -124,37 +98,67 @@
             this.fromLabel.TabIndex = 10;
             this.fromLabel.Text = "From : ";
             // 
+            // reportViewer1
+            // 
+            reportDataSource1.Name = "MostPerformed";
+            reportDataSource1.Value = this.spMostPerformedTestsBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "HealthCare.MostPerformedTests.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(3, 117);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(729, 427);
+            this.reportViewer1.TabIndex = 16;
+            // 
+            // _cs6232_g2DataSet
+            // 
+            this._cs6232_g2DataSet.DataSetName = "_cs6232_g2DataSet";
+            this._cs6232_g2DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // spMostPerformedTestsBindingSource
+            // 
+            this.spMostPerformedTestsBindingSource.DataMember = "spMostPerformedTests";
+            this.spMostPerformedTestsBindingSource.DataSource = this._cs6232_g2DataSet;
+            // 
+            // spMostPerformedTestsTableAdapter
+            // 
+            this.spMostPerformedTestsTableAdapter.ClearBeforeFill = true;
+            // 
+            // spMostPerformedTestsTableAdapter1
+            // 
+            this.spMostPerformedTestsTableAdapter1.ClearBeforeFill = true;
+            // 
             // MostPeformedTestsUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.reportViewer1);
             this.Controls.Add(this.instructionsLabel);
             this.Controls.Add(this.endDate);
             this.Controls.Add(this.startDate);
             this.Controls.Add(this.generateReportButton);
             this.Controls.Add(this.toLabel);
             this.Controls.Add(this.fromLabel);
-            this.Controls.Add(this.reportViewer1);
             this.Name = "MostPeformedTestsUserControl";
             this.Size = new System.Drawing.Size(735, 558);
+            ((System.ComponentModel.ISupportInitialize)(this._cs6232_g2DataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.spMostPerformedTestsBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.mostperformed)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
-        private System.Windows.Forms.BindingSource spMostPerformedTestsBindingSource;
-        private mostperformed mostperformed;
-        private mostperformedTableAdapters.spMostPerformedTestsTableAdapter spMostPerformedTestsTableAdapter;
         private System.Windows.Forms.Label instructionsLabel;
         private System.Windows.Forms.DateTimePicker endDate;
         private System.Windows.Forms.DateTimePicker startDate;
         private System.Windows.Forms.Button generateReportButton;
         private System.Windows.Forms.Label toLabel;
         private System.Windows.Forms.Label fromLabel;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource spMostPerformedTestsBindingSource;
+        private _cs6232_g2DataSet _cs6232_g2DataSet;
+        private _cs6232_g2DataSetTableAdapters.spMostPerformedTestsTableAdapter spMostPerformedTestsTableAdapter;
+        private mostperformedTableAdapters.spMostPerformedTestsTableAdapter spMostPerformedTestsTableAdapter1;
     }
 }
--- HealthCare/UserControls/MostPeformedTestsUserControl.cs
+++ HealthCare/UserControls/MostPeformedTestsUserControl.cs
@@ -19,14 +19,14 @@ namespace HealthCare.UserControls
 
         private void reportViewer1_Load(object sender, EventArgs e)
         {
-           this.spMostPerformedTestsTableAdapter.Fill(this.mostperformed.spMostPerformedTests, DateTime.Today, DateTime.Today);
+           this.spMostPerformedTestsTableAdapter.Fill(this._cs6232_g2DataSet.spMostPerformedTests, DateTime.Today, DateTime.Today);
            this.reportViewer1.RefreshReport();
             
         }
 
         private void generateReportButton_Click_1(object sender, EventArgs e)
         {
-            this.spMostPerformedTestsTableAdapter.Fill(this.mostperformed.spMostPerformedTests, this.startDate.Value, this.endDate.Value);
+            this.spMostPerformedTestsTableAdapter.Fill(this._cs6232_g2DataSet.spMostPerformedTests, this.startDate.Value, this.endDate.Value);
             this.reportViewer1.RefreshReport();
         }
     }
